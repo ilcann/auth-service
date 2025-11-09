42b697c046155b20a1fcdb5bea230501049c9a0d
ilcann <ilcan21@itu.edu.tr>
2025-11-09 18:49:16 +0300

    feat: implement departments and roles controllers with JWT guards

--- src/modules/departments/departments.controller.ts
+++ src/modules/departments/departments.controller.ts
@@ -1,4 +1,18 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
+import { DepartmentsService } from './departments.service';
+import { GetDepartmentsQueryDto } from './dto/get-departments-query.dto';
+import { GetDepartmentsResponseDto } from './dto/get-departments-response.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('departments')
-export class DepartmentsController {}
+@UseGuards(AuthGuard('jwt'))
+export class DepartmentsController {
+  constructor(private readonly departmentsService: DepartmentsService) {}
+
+  @Get()
+  async getDepartments(
+    @Query() query: GetDepartmentsQueryDto,
+  ): Promise<GetDepartmentsResponseDto> {
+    return await this.departmentsService.getDepartments(query);
+  }
+}
--- src/modules/roles/roles.controller.ts
+++ src/modules/roles/roles.controller.ts
@@ -1,9 +1,11 @@
-import { Controller, Get, Query } from '@nestjs/common';
+import { Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { RolesService } from './roles.service';
 import { GetRolesQueryDto } from './dto/get-roles-query.dto';
 import { GetRolesResponseDto } from './dto/get-roles-response.dto';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('roles')
+@UseGuards(AuthGuard('jwt'))
 export class RolesController {
   constructor(private readonly rolesService: RolesService) {}
 
