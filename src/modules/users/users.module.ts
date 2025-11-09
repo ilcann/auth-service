974664df6ade2ae59cd7a896411599ac108a7f5a
ilcann <ilcan21@itu.edu.tr>
2025-11-09 17:02:20 +0300

    refactor(users): migrate Prisma operations to repository layer

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -2,10 +2,11 @@ import { Module } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { PrismaModule } from 'src/database/prisma/prisma.module';
 import { UsersController } from './users.controller';
+import { UsersRepository } from './users.repository';
 
 @Module({
   imports: [PrismaModule],
-  providers: [UsersService],
+  providers: [UsersService, UsersRepository],
   exports: [UsersService],
   controllers: [UsersController],
 })
--- src/modules/users/users.service.ts
+++ src/modules/users/users.service.ts
@@ -1,34 +1,25 @@
 import { Injectable } from '@nestjs/common';
 import { IUsersService } from './interfaces/users-service.interface';
-import { PrismaService } from 'src/database/prisma/prisma.service';
 import { RegisterDto } from '../auth/dto/register.dto';
 import cryptoUtils from 'src/common/utils/crypto.util';
 import { User } from 'src/modules/auth/interfaces/user.type';
 import { UserStatus } from '@prisma/client';
+import { UsersRepository } from './users.repository';
 
 @Injectable()
 export class UsersService implements IUsersService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly usersRepository: UsersRepository) {}
 
   async findById(userId: string): Promise<User | null> {
-    return await this.prisma.user.findUnique({
-      where: { id: userId },
-      include: { role: true, department: true },
-    });
+    return await this.usersRepository.findById(userId);
   }
 
   async findByEmail(email: string): Promise<User | null> {
-    return await this.prisma.user.findUnique({
-      where: { email },
-      include: { role: true, department: true },
-    });
+    return await this.usersRepository.findByEmail(email);
   }
 
   async findByUsername(username: string): Promise<User | null> {
-    return await this.prisma.user.findUnique({
-      where: { username },
-      include: { role: true, department: true },
-    });
+    return await this.usersRepository.findByUsername(username);
   }
 
   async createUser(dto: RegisterDto): Promise<User> {
@@ -36,19 +27,12 @@ export class UsersService implements IUsersService {
 
     const passwordHash = await cryptoUtils.hashPlainText(password);
 
-    return await this.prisma.user.create({
-      data: {
-        ...rest,
-        passwordHash,
-        status: UserStatus.ACTIVE, // Geçici olarak ACTIVE yapıldı User manegement eklendiğinde değiştirilecek
-        role: {
-          connect: { key: 'admin' }, // Default role olarak 'admin' geçici olarak atanıyor seed.ts'deki role key değerine göre
-        },
-        department: {
-          connect: { key: 'general' }, // Default department olarak 'GENERAL' atanıyor seed.ts'deki department key değerine göre
-        },
-      },
-      include: { role: true, department: true },
+    return await this.usersRepository.create({
+      ...rest,
+      passwordHash,
+      status: UserStatus.ACTIVE, // Geçici olarak ACTIVE yapıldı User manegement eklendiğinde değiştirilecek
+      roleKey: 'admin', // Default role olarak 'admin' geçici olarak atanıyor seed.ts'deki role key değerine göre
+      departmentKey: 'general', // Default department olarak 'GENERAL' atanıyor seed.ts'deki department key değerine göre
     });
   }
 }
